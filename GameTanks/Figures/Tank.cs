36e9f658bd5bf35e0c643eaf5a86e05996ec757a
Волосович Сергей <volosovich_sergey@mail.ru>
2017-07-26 14:25:00 +0300

    Танки ездят

--- GameTanks/Figures/Tank.cs
+++ GameTanks/Figures/Tank.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Figures
 {
-    class Tank : FiguresAbstract
+    public class Tank : FiguresAbstract
     {
 
 
         public Tank(int x1, int y1, int x2, int y2, int x3, int y3, Color color) : base(x1, y1, x2, y2, x3, y3)
         {
-            this.speed = 1;
+            this.speed = 5;
             this.armor = 1;
             this.power = 2;
             this.live = 5;
@@ -27,28 +27,79 @@ namespace Figures
 
         public override void Move(String way)
         {
+            double centrx = (points[0].X + points[1].X + points[2].X) / 3,
+            centry = (points[0].Y + points[1].Y + points[2].Y) / 3,
+            d0 = Math.Sqrt(Math.Pow(centrx - points[0].X, 2) + Math.Pow(centry - points[0].Y, 2)),
+            dp = Math.Sqrt(Math.Pow(points[2].X - points[1].X, 2) + Math.Pow(points[2].Y - points[1].Y, 2))/2;
+
             if (way.Equals("up"))
             {
-                points[0].Y = points[0].Y - (this.speed);
-                points[1].Y = points[1].Y - (this.speed);
-                points[2].Y = points[2].Y - (this.speed);
+                if (points[0].Y < points[1].Y && points[0].Y<points[2].Y) {
 
+                    points[0].Y = points[0].Y - (this.speed);
+                    points[1].Y = points[1].Y - (this.speed);
+                    points[2].Y = points[2].Y - (this.speed);
+                }
+                else
+                {
+                    points[0].Y = (int)Math.Round(centry - d0);
+                    points[0].X = (int)Math.Round(centrx);
+                    points[1].X = (int)Math.Round(centrx + dp);
+                    points[2].X = (int)Math.Round(centrx - dp);
+                    points[1].Y = (int)Math.Round((centry * 3 - points[0].Y) / 2);
+                    points[2].Y = points[1].Y;
+                }
             }else if (way.Equals("down"))
             {
-                points[0].Y = points[0].Y + (this.speed);
-                points[1].Y = points[1].Y + (this.speed);
-                points[2].Y = points[2].Y + (this.speed);
+                if (points[0].Y > points[1].Y && points[0].Y > points[2].Y) {
+                    points[0].Y = points[0].Y + (this.speed);
+                    points[1].Y = points[1].Y + (this.speed);
+                    points[2].Y = points[2].Y + (this.speed);
+                }
+                else {
+                    points[0].Y = (int)Math.Round(centry + d0);
+                    points[0].X = (int)Math.Round(centrx);
+                    points[1].X = (int)Math.Round(centrx - dp);
+                    points[2].X = (int)Math.Round(centrx + dp);
+                    points[1].Y = (int)Math.Round((centry * 3 - points[0].Y) / 2);
+                    points[2].Y = points[1].Y;
+                }
             }
             else if (way.Equals("right"))
             {
-                points[0].X = points[0].X + (this.speed);
-                points[1].X = points[1].X + (this.speed);
-                points[2].X = points[2].X + (this.speed);
-            }else if (way.Equals("left"))
+                if (points[0].X > points[1].X && points[0].X > points[2].X)
+                {
+                    points[0].X = points[0].X + (this.speed);
+                    points[1].X = points[1].X + (this.speed);
+                    points[2].X = points[2].X + (this.speed);
+                }else
+                {
+                    points[0].X = (int)Math.Round(centrx + d0);
+                    points[0].Y = (int)Math.Round(centry);
+                    points[1].Y = (int)Math.Round(centry + dp);
+                    points[2].Y = (int)Math.Round(centry - dp);
+                    points[1].X = (int)Math.Round((centrx * 3 - points[0].X) / 2);
+                    points[2].X = points[1].X;
+                }
+            }
+            else if (way.Equals("left"))
             {
-                points[0].X = points[0].X - (this.speed);
-                points[1].X = points[1].X - (this.speed);
-                points[2].X = points[2].X - (this.speed);
+                if (points[0].X < points[1].X && points[0].X < points[2].X)
+                {
+                    points[0].X = points[0].X - (this.speed);
+                    points[1].X = points[1].X - (this.speed);
+                    points[2].X = points[2].X - (this.speed);
+                }
+                else
+                {
+                    points[0].X = (int)Math.Round(centrx - d0);
+                    points[0].Y = (int)Math.Round(centry);
+                    points[1].Y = (int)Math.Round(centry - dp);
+                    points[2].Y = (int)Math.Round(centry + dp);
+                    points[1].X = (int)Math.Round((centrx * 3 - points[0].X) / 2);
+                    points[2].X = points[1].X;
+                }
+                
             }
         }
 
---
+++ GameTanks/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Figures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        FiguresAbstract tank1, tank2;
+        Graphics formCanva;
+        public Form1()
+        {
+            InitializeComponent();
+            formCanva = CreateGraphics();
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'w')
+            {
+                tank1.Move("up");   
+            }else if (e.KeyChar == 's')
+            {
+                tank1.Move("down");
+            }
+            else if (e.KeyChar == 'a')
+            {
+                tank1.Move("left");
+            }
+            else if (e.KeyChar == 'd')
+            {
+                tank1.Move("right");
+            }else if (e.KeyChar == 'i')
+            {
+                tank2.Move("up");
+            }
+            else if (e.KeyChar == 'k')
+            {
+                tank2.Move("down");
+            }
+            else if (e.KeyChar == 'j')
+            {
+                tank2.Move("left");
+            }
+            else if (e.KeyChar == 'l')
+            {
+                tank2.Move("right");
+            }
+
+            formCanva.Clear(this.BackColor);
+            tank1.Draw(ref formCanva);
+            tank2.Draw(ref formCanva);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tank1 = new Tank(20, this.Height / 2, 10, this.Height / 2+10, 10, this.Height / 2-10, Color.Blue);
+            tank2 = new Tank(this.Width-40, this.Height / 2, this.Width - 30, this.Height / 2-10, this.Width - 30, this.Height / 2+10, Color.Red);
+            tank1.Draw(ref formCanva);
+            tank2.Draw(ref formCanva);
+        }
+    }
+}
